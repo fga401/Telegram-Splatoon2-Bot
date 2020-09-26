24635f23d7725e721312b15f2e4e5c2e9aeb601a
Lyew Marven <fga401@outlook.com>
2020-09-26 08:47:59 +0800

    fix: change table schema

--- migrate/sqls/01_create_tables.up.sql
+++ migrate/sqls/01_create_tables.up.sql
@@ -6,18 +6,17 @@ CREATE TABLE account (
 
 CREATE TABLE username (
     uid BIGINT NOT NULL,
-    iksm VARCHAR(32) NOT NULL,
+    user_name VARCHAR(32) NOT NULL,
     PRIMARY KEY (uid)
 );
 
 CREATE TABLE status (
     uid BIGINT NOT NULL,
-    iksm VARCHAR(32) NOT NULL,
+    iksm CHARACTER(40) NOT NULL,
     is_block BOOLEAN NOT NULL,
     max_account TINYINT NOT NULL,
     n_account TINYINT NOT NULL,
     is_admin BOOLEAN NOT NULL,
     allow_polling BOOLEAN NOT NULL,
-    current_iksm iksm VARCHAR(32) NOT NULL,
     PRIMARY KEY (uid)
 );
