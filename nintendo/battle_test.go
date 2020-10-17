5319e8e4c76fd699e0dfbfa8a4dfc9d0939cfdf2
Lyew Marven <fga401@outlook.com>
2020-10-17 08:45:20 +0800

    test: fix test file

--- nintendo/battle_test.go
+++ nintendo/battle_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestGetResults(t *testing.T) {
 	prepareTest()
-	_, err := GetResults("72811dcfc39d199333bc466549a80ac0ff06070d", "en-US")
+	_, err := GetResults("72811dcfc39d199333bc466549a80ac0ff06070d", 480,"en-US")
 	assert.Nil(t, err)
 }
 
